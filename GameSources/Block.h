ea25098b1d1deb78fb7034bb6778ef916acb9800
SANOSI\sanos <sanosuke_38@icloud.com>
2021-02-02 13:51:28 +0900

    GameSourcesにBlock.hとBlock.cppを追加した

---
+++ GameSources/Block.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class Block : public GameObject
+	{
+	public:
+		Block(const std::shared_ptr<Stage>& stage)
+			:GameObject(stage)
+		{
+		}
+	};
+
+}
